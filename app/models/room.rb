1d97015395b66015b28379707dca15fc735384f2
Patryk <patryk.dabrowski@decathlon.com>
2017-07-25 00:49:25 +0200

    Creating validation for params

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ActiveRecord::Base
-
-
+	validates :title, presence: true,
+	validates :name, presence: true,
+	validates :person, presence: true,
+	validates :start, presence: true,
+	validates :end, presence: true,
+	validates :title, presence: true,
 end
